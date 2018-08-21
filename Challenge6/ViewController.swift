3382cd72ce54ec38d4994149bac84e17bc49a201
Jethro Williams <jethrowilliams4@hotmail.com>
2018-08-21 16:05:23 +0100

    Initial Commit

---
+++ Challenge6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Challenge6
+//
+//  Created by Jethro Williams on 21/08/2018.
+//  Copyright © 2018 Jethro Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
