ae448429c9b244972bc4e8da618e056cfc027413
Jethro Williams <jethrowilliams4@hotmail.com>
2018-08-24 00:39:26 +0100

    Add 'Countries.json' file and 'swiftyJSON' file, add tableViewController in IB, add 'numberOfRowsInSection' and 'cellForRowAt' methods, and add method to parse data from json file into dictionary.

--- Challenge6/ViewController.swift
+++ Challenge6/ViewController.swift
@@ -1,18 +1,55 @@
-//
-//  ViewController.swift
-//  Challenge6
-//
-//  Created by Jethro Williams on 21/08/2018.
-//  Copyright © 2018 Jethro Williams. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var countries = [[String: String]]()
+    var numberOfCountries = 10
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        title = "Select country"
+        
+        if let jsonPath = Bundle.main.path(forResource: "Countries", ofType: "json") {
+            print("jsonPath: \(jsonPath)")
+            
+            if let data = try? String(contentsOfFile: jsonPath) {
+                
+                let json = JSON(parseJSON: data)
+                print("json: \(json)")
+                
+                parse(json: json)
+                
+                print(countries)
+                
+            } else {
+                print("Unable to get contents of JSON file")
+            }
+        }
+
+    }
+    
+    func parse(json: JSON) {
+        for result in json.arrayValue {
+            let countryName = result["countryName"].stringValue
+            let continent = result["continent"].stringValue
+            let population = result["population"].stringValue
+            let currency = result["currency"].stringValue
+            
+            let obj = ["countryName": countryName, "continent": continent, "population": population, "currency": currency]
+            
+            countries.append(obj)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberOfCountries
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+        cell.textLabel?.text = "Country name"
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
